503f7c54ee5bef5a17dbc0c0a206c3b59756ba65
xseignard <xavier.seignard@gmail.com>
2013-08-14 08:11:17 +0000

    Updating some documentation style

--- src/lib/gpio.js
+++ src/lib/gpio.js
@@ -18,18 +18,16 @@ var gpioEventNames = {
  *
  * Here is a quick sample: 
  * 
- * ```js
- * var pin8 = new GPIO(8, 'in');
- * pin8.on('ready', function() {
- *  console.log('ready');
- * });
- * pin8.on('rise', function() {
- *  console.log('rise');
- * });
- * pin8.on('change', function(value) {
- *  console.log(value);
- * });
- * ```
+ *   var pin8 = new GPIO(8, 'in');
+ *   pin8.on('ready', function() {
+ *   	console.log('ready');
+ *   });
+ *   pin8.on('rise', function() {
+ *   	console.log('rise');
+ *   });
+ *   pin8.on('change', function(value) {
+ *   	console.log(value);
+ *   });
  *
  * @constructor
  * @property {Number} pinNumber - GPIO pin number according to wiringPi
